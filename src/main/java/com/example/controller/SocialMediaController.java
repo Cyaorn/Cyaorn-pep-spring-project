e6d72381984c0d444c9f8f8726bca5b74e549184
Ciaran Diep <diepc153@gmail.com>
2023-09-27 14:56:45 +0000

    added more specific annotations for each endpoint

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -3,8 +3,12 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.entity.Account;
@@ -30,43 +34,43 @@ public class SocialMediaController {
         this.messageService = ms;
     }
 
-    @RequestMapping(path="/register", method=RequestMethod.POST)
+    @PostMapping("/register")
     public @ResponseBody Account registerUser() {
         return null;
     }
 
-    @RequestMapping(path="/login", method=RequestMethod.POST)
+    @PostMapping("/login")
     public @ResponseBody Account loginUser() {
         return null;
     }
 
-    @RequestMapping(path="/messages", method=RequestMethod.POST)
+    @PostMapping("/messages")
     public @ResponseBody Message createMessage() {
         return null;
     }
 
-    @RequestMapping(path="/messages", method=RequestMethod.GET)
+    @GetMapping("/messages")
     public @ResponseBody List<Message> getAllMessages() {
         return null;
     }
 
-    @RequestMapping(path="/messages/{message_id}", method=RequestMethod.GET)
-    public @ResponseBody Message getMessageById() {
+    @GetMapping("/messages/{message_id}")
+    public @ResponseBody Message getMessageById(@PathVariable int message_id) {
         return null;
     }
 
-    @RequestMapping(path="/messages/{message_id}", method=RequestMethod.DELETE)
-    public @ResponseBody Message deleteMessage() {
+    @DeleteMapping("/messages/{message_id}")
+    public @ResponseBody Message deleteMessage(@PathVariable int message_id) {
         return null;
     }
 
-    @RequestMapping(path="/messages", method=RequestMethod.PATCH)
+    @PatchMapping("/messages")
     public @ResponseBody Message updateMessage() {
         return null;
     }
 
-    @RequestMapping(path="/accounts/{account_id}", method=RequestMethod.GET)
-    public @ResponseBody List<Message> getAllMessagesByUser() {
+    @GetMapping("/accounts/{account_id}")
+    public @ResponseBody List<Message> getAllMessagesByUser(@PathVariable int account_id) {
         return null;
     }
 }
